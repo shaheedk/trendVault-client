4e1fafbaeb0d3fb5d8dd8a8b49009e7f26d4b08d
Shaheed mon <sheheedk07@gmail.com>
2025-06-19 22:39:26 +0530

    ongoing

--- src/pages/Placeorder.tsx
+++ src/pages/Placeorder.tsx
@@ -1,6 +1,14 @@
+import { useContext, useState } from "react";
+import { assets } from "../assets/assets";
+import CartTotal from "../components/elements/CartTotal";
 import Title from "../components/elements/Title";
+import { ShopContext } from "../context/ShopContext";
+
 
 const Placeorder = () => {
+  const [method, setmethod] = useState("cod");
+
+  const {navigate}=useContext(ShopContext)
   return (
     <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80h] border-t">
       {/* left side  */}
@@ -9,12 +17,86 @@ const Placeorder = () => {
           <Title text1="DELIVERY" text2="INFORMATION" />
         </div>
         <div className="flex gap-3">
-          <input type="text" placeholder="First name" className="border border-gray-100 rounded py-1.5 px-3.5 w-full" />
-          <input type="text" placeholder="Last name" className="border border-gray-100 rounded py-1.5 px-3.5 w-full" />
-
+          <input
+            type="text"
+            placeholder="First name"
+            className="border border-gray-100 rounded py-1.5 px-3.5 w-full"
+          />
+          <input
+            type="text"
+            placeholder="Last name"
+            className="border border-gray-100 rounded py-1.5 px-3.5 w-full"
+          />
         </div>
-          <input type="text" placeholder="E-mail address" className="border border-gray-100 rounded py-1.5 px-3.5 w-full" />
+        <input
+          type="text"
+          placeholder="E-mail address"
+          className="border border-gray-100 rounded py-1.5 px-3.5 w-full"
+        />
+        <input
+          type="text"
+          placeholder="Street"
+          className="border border-gray-100 rounded py-1.5 px-3.5 w-full"
+        />
+        <div className="flex gap-3">
+          <input
+            type="text"
+            placeholder="City"
+            className="border border-gray-100 rounded py-1.5 px-3.5 w-full"
+          />
+          <input
+            type="text"
+            placeholder="State"
+            className="border border-gray-100 rounded py-1.5 px-3.5 w-full"
+          />
+        </div>
+        <div className="flex gap-3">
+          <input
+            type="number"
+            placeholder="Zipcode"
+            className="border border-gray-100 rounded py-1.5 px-3.5 w-full"
+          />
+          <input
+            type="text"
+            placeholder="Country"
+            className="border border-gray-100 rounded py-1.5 px-3.5 w-full"
+          />
+        </div>
+        <input
+          type="number"
+          placeholder="Phone"
+          className="border border-gray-100 rounded py-1.5 px-3.5 w-full"
+        />
+      </div>
+      {/*------------ Right side ------------- */}
+      <div className="mt-8">
+        <div className="mt-8 min-w-80">
+          <CartTotal />
+        </div>
+        <div className="mt-12">
+          <Title text1="PAYMENT" text2="METHOD" />
 
+          {/* -------------Payment Method selection --------------- */}
+          <div className="flex flex-col lg:flex-row ">
+            <div  onClick={()=>setmethod('stripe')} className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
+              <p className={`min-w-3.5 h-3.5 border rounded-full  ${method==='stripe'? 'bg-orange-600 border-none':''}`}></p>
+              <img className="h-5 mx-4" src={assets.stripe_logo} alt="" />
+            </div>
+            <div onClick={()=>setmethod('razorpay')} className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method==='razorpay'? 'bg-orange-600 border-none':''}`}></p>
+              <img className="h-5 mx-4" src={assets.razorpay_logo} alt="" />
+            </div>
+            <div onClick={()=>setmethod('cod')} className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method==='cod'? 'bg-orange-600 border-none':''}`}></p>
+              <p className="text-gray-500 text-sm font-medium mx-4">
+                CASH ON DELIVERY
+              </p>
+            </div>
+          </div>
+          <div className="w-full text-end mt-8 ">
+            <button onClick={()=>navigate('/orders')} className="bg-black text-white px-16 py-3 text-sm">PLACE ORDER</button>
+          </div>
+        </div>
       </div>
     </div>
   );
