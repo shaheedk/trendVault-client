93befe31cf729b4cd10d86df3891d2e0553a2ce0
Shaheed mon <sheheedk07@gmail.com>
2025-06-20 00:04:32 +0530

    completed

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,6 @@
 import { assets } from "../assets/assets";
 import Title from "../components/elements/Title";
+import Newsletter from "../components/newslet/Newsletter";
 
 const About = () => {
   return (
@@ -38,6 +39,27 @@ const About = () => {
           </p>
         </div>
       </div>
+      <div className="text-4xl py-4 ">
+        <Title text1="WHY" text2="CHOOSE US"/>
+      </div>
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 flex flex-co gap-5">
+          <b>Quality Assurance:</b>
+          <p className="text-gray-600 ">We meticulously select and vet each product to ensure it meets our stringent quality standards.</p>
+
+        </div>
+        <div className="border px-10 md:px-16 py-8 flex flex-co gap-5">
+          <b>Conveniece:</b>
+          <p className="text-gray-600 ">With our user-friendly interface and hassle-free ordering process, shopping has never been easier.</p>
+
+        </div>
+ <div className="border px-10 md:px-16 py-8 flex flex-co gap-5">
+          <b>Exceptional Customer Service:</b>
+          <p className="text-gray-600 ">Our team of dedicated professionals is here to assist you the way, ensuring your satisfaction is our top priority.</p>
+
+        </div>
+      </div>
+      <Newsletter/>
     </div>
   );
 };
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,8 +1,25 @@
+import { assets } from "../assets/assets"
+import Title from "../components/elements/Title"
+import Newsletter from "../components/newslet/Newsletter"
 
 const Contact = () => {
   return (
     <div>
-      
+      <div className="text-center text-2xl pt-10 border-t">
+        <Title text1="CONTACT" text2="US"/>
+      </div>
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img src={assets.contact_img} alt=""  className="w-full md:max-w-[480px]"/>
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-semibold text-xl text-gray-600">our Store</p>
+          <p className="text-gray-500">080833 fsflkjfsda <br /> kochi, kerala,india </p>
+          <p className="text-gray-500">Tel:(+91) 8904532053 <br />Emai:admin@gmail.com</p>
+          <p  className="font-semibold text-xl text-gray-600">Careers at zipCart</p>
+          <p  className="text-gray-500">Learn more about our team and job openings. </p>
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">Explore Jobs</button>
+        </div>
+      </div>
+      <Newsletter/>
     </div>
   )
 }
